33a1b991cd5955b0b0516aba9a4682473a110796
pranesh200425 <danieltup10@gmail.com>
2025-08-22 14:16:43 +0530

    now the program can recursively search through the subdirectories

--- quickSearch.c
+++ quickSearch.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <dirent.h>
 #include <string.h>
+#include <unistd.h> // For getcwd() on Unix-like systems
+#include <sys/stat.h>
+#include <sys/types.h>
 
 #define file_size 2000000
 
@@ -41,15 +44,18 @@ void writeFile(void)
     {
         fwrite(my_array, sizeof(unsigned char), file_size, file_out);
         fclose(file_out);
-        printf("Save complete.\n");
+        // printf("Save complete.\n");
     }
 }
-void readfiles(unsigned char *array)
+void readfiles(unsigned char *array, char *basePath)
 {
+    char path[1000];
     unsigned long array_size_in_bits = 16000000;
     struct dirent *de;
-
-    DIR *dr = opendir(".");
+    /* char basePath[1024]; */
+    // printf("basepath at readfiles%s",basePath);
+    // exit(0);
+    DIR *dr = opendir(basePath);
     if (dr == NULL)
     {
         printf("Error reading directory\n");
@@ -57,34 +63,50 @@ void readfiles(unsigned char *array)
     }
     while ((de = readdir(dr)) != NULL)
     {
-        if ((strcmp(de->d_name, ".") == 0) || (strcmp(de->d_name, "..") == 0))
+        /* if ((strcmp(de->d_name, ".") == 0) || (strcmp(de->d_name, "..") == 0))
         {
             continue;
-        }
-        unsigned long h1 = hash_one(de->d_name);
-        unsigned long h2 = hash_two(de->d_name);
-
-        for (int i = 0; i < 6; i++)
+        } */
+        if (strcmp(de->d_name, ".") != 0 && strcmp(de->d_name, "..") != 0 && strcmp(de->d_name, ".git") != 0)
         {
-            unsigned long combined_hash = h1 + (i * h2);
-            unsigned long bit_index = combined_hash % array_size_in_bits;
-            unsigned long byte_index = bit_index / 8;
-            int bit_position = bit_index % 8;
+            snprintf(path, sizeof(path), "%s/%s", basePath, de->d_name);
+            struct stat statbuf;
 
-            array[byte_index] |= (1 << bit_position);
+            if (stat(path, &statbuf) == 0 && S_ISDIR(statbuf.st_mode))
+            {
+                readfiles(array, path);
+            }
+            else
+            {
+                //printf("%s\n", de->d_name);
+                printf("Indexing: %s\n", path);
+                unsigned long h1 = hash_one(de->d_name);
+                unsigned long h2 = hash_two(de->d_name);
+
+                for (int i = 0; i < 6; i++)
+                {
+                    unsigned long combined_hash = h1 + (i * h2);
+                    unsigned long bit_index = combined_hash % array_size_in_bits;
+                    unsigned long byte_index = bit_index / 8;
+                    int bit_position = bit_index % 8;
+
+                    array[byte_index] |= (1 << bit_position);
+                }
+            }
         }
     }
-    writeFile();
+    closedir(dr);
 }
 
-void loadbin(void)
+void loadbin(char *basePath)
 {
+    writeFile();
     my_array = malloc(file_size * sizeof(unsigned char));
     FILE *file_pointer = fopen("my_filter.bin", "rb");
-
     fread(my_array, sizeof(unsigned char), file_size, file_pointer);
     fclose(file_pointer);
-    readfiles(my_array);
+    readfiles(my_array, basePath);
+    writeFile();
     free(my_array);
 }
 
@@ -92,7 +114,6 @@ void search(char *file, unsigned char *array)
 {
     my_array = malloc(file_size * sizeof(unsigned char));
     unsigned long array_size_in_bits = 16000000;
-    // printf("%s\n", file);
     FILE *file_pointer = fopen("my_filter.bin", "rb");
 
     if (file_pointer == NULL)
@@ -128,6 +149,8 @@ void search(char *file, unsigned char *array)
 
 void main(int argc, char **argv)
 {
+    char basePath[1024];
+    getcwd(basePath, sizeof(basePath)) != NULL;
     if (argc == 1)
     {
         printf("Use --index-files to index the files\n Use --search [filename] to search for files");
@@ -135,8 +158,8 @@ void main(int argc, char **argv)
     }
     if (strcmp("--index-files", argv[1]) == 0)
     {
-        printf("Files are being indexed. please wait...\n");
-        loadbin();
+        // printf("Files are being indexed. please wait...\n");
+        loadbin(basePath);
     }
     else if (strcmp("--search", argv[1]) == 0)
         search(argv[2], my_array);
